12b11bddcd3021786b1c2dbff77f1855bc0230ad
Ananye Shobhith Gajula <ananyeshobhithgajula@gmail.com>
2025-07-19 17:02:11 -0400

    Delete 22_Linkedin_DuplicateJobListings_Easy.sql

--- 22_Linkedin_DuplicateJobListings_Easy.sql
+++
@@ -1,7 +0,0 @@
-WITH duplics AS(SELECT company_id, COUNT(job_id) AS count_of_id
-FROM job_listings
-GROUP BY company_id
-HAVING COUNT(job_id)>1
-ORDER BY 2 DESC)
-SELECT COUNT(*) AS duplicate_companies
-FROM duplics;
