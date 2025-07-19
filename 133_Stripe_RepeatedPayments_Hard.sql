b973c50a84bb49840fc6d2100dcf2e8490493886
Ananye Shobhith Gajula <ananyeshobhithgajula@gmail.com>
2025-07-19 17:00:55 -0400

    Delete 133_Stripe_RepeatedPayments_Hard.sql

--- 133_Stripe_RepeatedPayments_Hard.sql
+++
@@ -1,9 +0,0 @@
-WITH abc AS (SELECT *, 
-LAG(amount) OVER (PARTITION BY credit_card_id ORDER BY transaction_id) AS lag_amt,
-LAG(transaction_timestamp) OVER 
-(PARTITION BY credit_card_id ORDER BY transaction_id) AS lag_time
-FROM transactions)
-SELECT COUNT(*) AS payment_count
-FROM abc 
-WHERE amount=lag_amt AND 
-EXTRACT(EPOCH FROM (transaction_timestamp-lag_time))/60 <=10;
