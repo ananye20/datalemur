0c4be94920503745838544841c1c40209ee5c980
Ananye Shobhith Gajula <ananyeshobhithgajula@gmail.com>
2025-07-19 17:00:20 -0400

    Delete 09_Microsoft_TeamPowerUsers_Easy.sql

--- 09_Microsoft_TeamPowerUsers_Easy.sql
+++
@@ -1,7 +0,0 @@
-SELECT sender_id, COUNT(message_id) AS message_count
-FROM messages
-WHERE EXTRACT(MONTH FROM sent_date)=8 
-AND EXTRACT(YEAR FROM sent_date)=2022
-GROUP BY sender_id
-ORDER BY 2 DESC
-LIMIT 2;
